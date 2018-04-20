66bf6d370098749d72edc9c25e8900184411401a
kwangmin <rkm92@naver.com>
2018-04-20 17:49:49 +0900

    linkedlist 최종

--- Day0420_Collections/src/linkedlist/MyListTest.java
+++ Day0420_Collections/src/linkedlist/MyListTest.java
@@ -1,48 +1,53 @@
 package linkedlist;
 
+import java.util.Scanner;
+
 public class MyListTest {
 	public static void main(String[] args) {
 		MyLinkedList list = new MyLinkedList();
-		list.add("A");
-		list.add("B");
+		Scanner in = new Scanner(System.in);	// 테스트용 scanner 객체
 		
-		System.out.println(list);
-		System.out.println("=================");
-
-		list.set(0, "F");
-		System.out.println(list);
-		System.out.println("=================");
-		
-		list.remove(0);
-		System.out.println(list);
-		System.out.println("=================");
-
-		list.remove(0);
-		System.out.println(list);
-		System.out.println("=================");
-
-		list.add("B");
-		System.out.println(list);
-		System.out.println("=================");
-		list.add("C");
-		System.out.println(list);
-		System.out.println("=================");
-		list.add("D");
-		System.out.println(list);
-		System.out.println("=================");
-		list.set(2,"E");
-		System.out.println(list);
-		System.out.println("=================");
-		list.remove(2);
-		System.out.println(list);
-		System.out.println("=================");
-		list.set(2, "F");
-		System.out.println(list);
-		System.out.println("=================");
-		list.set(1, "F");
-		System.out.println(list);
-		System.out.println("=================");
-		
-		System.out.println(list.get(1));
+		int num = 0;
+		loop:
+		while(num != -1) {
+			System.out.println("[1]노드추가 [2]노드set [3]노드삭제 [4]get노드 [5]사이즈 [6]노드출력");
+			System.out.print("입력 : ");
+			num = in.nextInt();
+			String str;
+			int index;
+			switch(num) {
+			case 1:
+				System.out.print("넣을 데이터입력 : ");
+				str = in.next();
+				list.add(str);
+				break;
+			case 2:
+				System.out.print("넣을 인덱스 입력 : ");
+				index = in.nextInt();
+				System.out.print("넣을 데이터입력 : ");
+				str = in.next();
+				list.set(index, str);
+				break;
+			case 3:
+				System.out.print("삭제 할 노드 인덱스 : ");
+				index = in.nextInt();
+				list.remove(index);
+				break;
+			case 4:
+				System.out.print("가져 올 노드 인덱스 : ");
+				index = in.nextInt();
+				System.out.println("가져온 노드 : "+list.get(index));
+				break;
+			case 5:
+				System.out.println("노드 사이즈 : "+list.size());
+				break;
+			case 6:
+				System.out.println(list);
+				break;
+			default:
+				break loop;
+			}
+			System.out.println();
+		}
 	}
 }
